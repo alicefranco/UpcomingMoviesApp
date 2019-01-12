42c4f90d5954fe22ebd447a937e9ce2ad7a11d2f
alicefranco <franco.lice@gmail.com>
2019-01-12 20:30:27 -0300

    Creation of MovieWS and BaseRequest classes to add coroutines to API calls

--- code-challenge-android-kotlin/app/src/main/java/com/arctouch/codechallenge/home/HomeActivity.kt
+++ code-challenge-android-kotlin/app/src/main/java/com/arctouch/codechallenge/home/HomeActivity.kt
@@ -1,30 +1,32 @@
 package com.arctouch.codechallenge.home
 
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.view.View
 import com.arctouch.codechallenge.R
 import com.arctouch.codechallenge.api.TmdbApi
 import com.arctouch.codechallenge.base.BaseActivity
+import com.arctouch.codechallenge.base.BaseRequest
 import com.arctouch.codechallenge.data.Cache
+import com.arctouch.codechallenge.webservice.MoviesWS
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.home_activity.*
 
-class HomeActivity : BaseActivity() {
+class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.home_activity)
 
-        api.upcomingMovies(TmdbApi.API_KEY, TmdbApi.DEFAULT_LANGUAGE, 1, TmdbApi.DEFAULT_REGION)
-            .subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe {
-                val moviesWithGenres = it.results.map { movie ->
-                    movie.copy(genres = Cache.genres.filter { movie.genreIds?.contains(it.id) == true })
-                }
-                recyclerView.adapter = HomeAdapter(moviesWithGenres)
-                progressBar.visibility = View.GONE
+        MoviesWS().getUpcomingMovies(1, {
+            val moviesWithGenres = it.results.map { movie ->
+                movie.copy(genres = Cache.genres.filter { movie.genreIds?.contains(it.id) == true })
             }
+            recyclerView.adapter = HomeAdapter(moviesWithGenres)
+            progressBar.visibility = View.GONE
+        },{
+            //TODO onError
+        })
     }
 }
---
+++ code-challenge-android-kotlin/app/src/main/java/com/arctouch/codechallenge/webservice/MoviesWS.kt
@@ -0,0 +1,66 @@
+package com.arctouch.codechallenge.webservice
+
+import com.arctouch.codechallenge.api.TmdbApi
+import com.arctouch.codechallenge.base.BaseRequest
+import com.arctouch.codechallenge.model.GenreResponse
+import com.arctouch.codechallenge.model.UpcomingMoviesResponse
+import kotlinx.coroutines.experimental.android.UI
+import kotlinx.coroutines.experimental.launch
+import ru.gildor.coroutines.retrofit.Result
+import ru.gildor.coroutines.retrofit.awaitResult
+
+class MoviesWS {
+
+    fun getGenres(onSuccess: (genres: GenreResponse) -> Unit, onError: () -> Unit) {
+        launch(UI){
+            try {
+                BaseRequest.api.genres(TmdbApi.API_KEY, TmdbApi.DEFAULT_LANGUAGE)
+                        .awaitResult().let { result ->
+                    when (result) {
+                        is Result.Ok -> onSuccess(result.value)
+                        is Result.Error -> {}//todo onError(result.response.code())
+                        is Result.Exception -> {}//todo onError(Constants.ERROR_BAD_REQUEST)
+                    }
+                }
+            } catch (e: Throwable) {
+                e.printStackTrace()
+                //todo onError() Constants.ERROR_BAD_REQUEST)
+            }
+        }
+    }
+
+    fun getUpcomingMovies(page: Long, onSuccess: (movies: UpcomingMoviesResponse) -> Unit, onError: () -> Unit){
+        launch(UI){
+            try {
+                BaseRequest.api.upcomingMovies(TmdbApi.API_KEY, TmdbApi.DEFAULT_LANGUAGE, page,
+                        TmdbApi.DEFAULT_REGION).awaitResult().let { result ->
+                    when (result) {
+                        is Result.Ok -> onSuccess(result.value)
+                        is Result.Error -> {}//todo onError(result.response.code())
+                        is Result.Exception -> {}//todo onError(Constants.ERROR_BAD_REQUEST)
+                    }
+                }
+            } catch (e: Throwable) {
+                e.printStackTrace()
+                //todo onError() Constants.ERROR_BAD_REQUEST)
+            }
+        }
+    }
+
+    fun getMovie(id: Long, onSuccess: () -> Unit, onError: () -> Unit){
+        launch(UI){
+            try {
+                BaseRequest.api.movie(id, TmdbApi.API_KEY, TmdbApi.DEFAULT_LANGUAGE).awaitResult().let { result ->
+                    when (result) {
+                        is Result.Ok -> {} //todo onSuccess(result.value)
+                        is Result.Error -> {}//todo onError(result.response.code())
+                        is Result.Exception -> {}//todo onError(Constants.ERROR_BAD_REQUEST)
+                    }
+                }
+            } catch (e: Throwable) {
+                e.printStackTrace()
+                //todo onError() Constants.ERROR_BAD_REQUEST)
+            }
+        }
+    }
+}
