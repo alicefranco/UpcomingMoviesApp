82d7d22dd21da7009b59d23259400cfbf86b21fe
alicefranco <franco.lice@gmail.com>
2019-01-13 00:15:14 -0300

    Implementation of HomeViewModel to retain data showed in the HomeActivity

---
+++ code-challenge-android-kotlin/app/src/main/java/com/arctouch/codechallenge/HomeViewModel.kt
@@ -0,0 +1,32 @@
+package com.arctouch.codechallenge
+
+import android.arch.lifecycle.LiveData
+import android.arch.lifecycle.MutableLiveData
+import android.arch.lifecycle.ViewModel
+import com.arctouch.codechallenge.data.Cache
+import com.arctouch.codechallenge.model.Movie
+import com.arctouch.codechallenge.webservice.MoviesWS
+
+class HomeViewModel : ViewModel() {
+    private lateinit var movies: MutableLiveData<List<Movie>>
+
+    fun getMovies(): LiveData<List<Movie>> {
+        if (!::movies.isInitialized) {
+            movies = MutableLiveData()
+            loadMovies()
+        }
+        return movies
+    }
+
+    private fun loadMovies()  {
+        MoviesWS().getUpcomingMovies(1, {
+            val moviesWithGenres = it.results.map { movie ->
+                movie.copy(genres = Cache.genres.filter { movie.genreIds?.contains(it.id) == true })
+            }
+            movies.value = moviesWithGenres
+        },{
+            //TODO onError()
+        })
+    }
+
+}
--- code-challenge-android-kotlin/app/src/main/java/com/arctouch/codechallenge/home/HomeActivity.kt
+++ code-challenge-android-kotlin/app/src/main/java/com/arctouch/codechallenge/home/HomeActivity.kt
@@ -1,13 +1,18 @@
 package com.arctouch.codechallenge.home
 
+import android.arch.lifecycle.Observer
+import android.arch.lifecycle.ViewModelProvider
+import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
+import com.arctouch.codechallenge.HomeViewModel
 import com.arctouch.codechallenge.R
 import com.arctouch.codechallenge.api.TmdbApi
 import com.arctouch.codechallenge.base.BaseActivity
 import com.arctouch.codechallenge.base.BaseRequest
 import com.arctouch.codechallenge.data.Cache
+import com.arctouch.codechallenge.model.Movie
 import com.arctouch.codechallenge.webservice.MoviesWS
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
@@ -19,14 +24,16 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.home_activity)
 
-        MoviesWS().getUpcomingMovies(1, {
-            val moviesWithGenres = it.results.map { movie ->
-                movie.copy(genres = Cache.genres.filter { movie.genreIds?.contains(it.id) == true })
+        val viewModel = ViewModelProviders.of(this).get(HomeViewModel::class.java)
+
+        viewModel.getMovies().observe(this, Observer<List<Movie>>{ movies ->
+            movies?.let{
+                recyclerView.adapter = HomeAdapter(movies)
+                progressBar.visibility = View.GONE
             }
-            recyclerView.adapter = HomeAdapter(moviesWithGenres)
-            progressBar.visibility = View.GONE
-        },{
-            //TODO onError
+            //TODO notify user if movies = null
         })
+
+
     }
 }
